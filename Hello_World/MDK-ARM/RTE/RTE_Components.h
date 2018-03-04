e6a5c82a44e430d954d743718be2b02c4a1046fc
Byzya <byzgaevrtc@gmail.com>
2018-03-04 23:46:59 +0300

    add first project

---
+++ Hello_World/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Hello_World' 
+ * Target:  'Hello_World' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
